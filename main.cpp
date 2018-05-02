fcd7b77b5e78c93043186f951b29c169ce168894
Gonçalo Oliveira <goncalo.oli@outlook.com>
2018-05-02 15:57:00 +0100

    Fixed merge conflict

--- main.cpp
+++ main.cpp
@@ -1,30 +1,19 @@
 #include <iostream>
 #include <algorithm>
-#include <unistd.h>
-#include <ctime>
-#include <chrono>
 #include "Board.h"
-#include "colorizer.h"
-
-using namespace std;
-using namespace std::chrono;
+#include "utils.h"
+#include "Puzzle.h"
 
 //TODO: We're gonna need this eventually
 //Takes a string and puts all its characters in uppercase
-void capitalize(string &word)
-{
-    transform(word.begin(), word.end(), word.begin(), [] (unsigned char c) { return toupper(c); } );
-}
+//void capitalize(string &word)
+//{
+//    transform(word.begin(), word.end(), word.begin(), [] (unsigned char c) { return toupper(c); } );
+//}
 
 int main()
 {
-    Board b(10,10);
-
-    Board::coord a('A', 'a');
-
-    b.showBoard();
-
-    b.showBoard();
+    Puzzle p;
 
     return 0;
 }
